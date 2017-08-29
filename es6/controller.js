ccccae75673db772eda08635f891af18bb69cee6
Tabitha Goodall <tg.twitchett@gmail.com>
2017-08-29 21:44:26 +0200

    refactoring table initialization logic

--- es6/controller.js
+++ es6/controller.js
@@ -1,4 +1,4 @@
-const NUM_ITEMS = 12
+const TOTAL_ROWS = 12
 const PRECISION = 6
 const COLUMN_MAPPING = {
     0: "name",
@@ -12,17 +12,17 @@ const COLUMN_MAPPING = {
 
 function initializeTable () {
     let table = document.getElementById("table")
-    let rows = Array(NUM_ITEMS) 
+    Array(TOTAL_ROWS)
         .fill()
         .forEach(item => table.append(createEmptyRow()))
 }
 
 function createEmptyRow () {
-    let row = document
-        .getElementById("row-template")
-        .cloneNode(true) // true -> deep clone
+    let row = document.createElement("tr")
     row.className = "row"
-    row.id = ""
+    Object.keys(COLUMN_MAPPING).forEach(_ => {
+        row.append(document.createElement("td"))
+    })
     return row
 }
 
@@ -34,23 +34,15 @@ function updateValues (data) {
 }
 
 function setValues (row, data) {
-    let cells = row.getElementsByTagName('td')
+    let cells = row.getElementsByTagName("td")
     Array.from(cells)
         .forEach((cell, i) => {
-            cells[i].innerHTML = getMappedValue(data, i)
+            cells[i].innerHTML = getValueForColumn(i, data)
         })
-
-    // cells[0].innerHTML = data.name
-    // cells[1].innerHTML = data.bestBid
-    // cells[2].innerHTML = data.bestAsk
-    // cells[3].innerHTML = data.lastChangeBid
-    // cells[4].innerHTML = data.lastChangeAsk
-    // cells[5].innerHTML = data.openBid
-    // cells[6].innerHTML = data.openAsk
 }
 
-function getMappedValue(data, i) {
-    let key = COLUMN_MAPPING[i]
+function getValueForColumn (col, data) {
+    let key = COLUMN_MAPPING[col]
     if (key !== "name") {
         return parseFloat(data[key]).toFixed(PRECISION)
     } else {
--- es6/main.js
+++ es6/main.js
@@ -3,28 +3,29 @@ import model from "./model.js"
 
 const STOMP_SERVER_URL = "ws://localhost:8011/stomp"
 const PRICES_ENDPOINT = "/fx/prices"
+
 const client = Stomp.client(STOMP_SERVER_URL)
 
-function connectHandler() {
+function connectHandler () {
     controller.initializeTable()
     let subscription = client.subscribe(PRICES_ENDPOINT, pricesResponseHandler)
     // TODO: unsubscribe
 }
 
-function pricesResponseHandler(response) {
+function pricesResponseHandler (response) {
     let body = JSON.parse(response.body)
     model.addItem(body)
     model.sortData()
     controller.updateValues(model.getData())
 }
 
-function errorHandler(error) {
+function errorHandler (error) {
   alert(error.headers.message)
 }
 
 client.connect({}, connectHandler, errorHandler)
 
-client.debug = function(msg) {
+client.debug = function (msg) {
     if (global.DEBUG) {
         console.info(msg)
     }
